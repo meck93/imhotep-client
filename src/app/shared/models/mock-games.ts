5bc1ead509ff747e2eff22378405641ee869e236
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2017-03-21 20:27:55 +0100

    #7 added start game button

--- src/app/shared/models/mock-games.ts
+++ src/app/shared/models/mock-games.ts
@@ -3,10 +3,9 @@
  */
 import { Game } from "./game";
 
-
 export const GAMES: Game[] = [
-    { id: 1, status: 'running', playerCount: 2 },
-    { id: 2, status: 'full', playerCount: 4 },
-    { id: 3, status: 'waiting for players', playerCount: 2 },
-    { id: 4, status: 'waiting for players', playerCount: 1 },
+    { id: 1, status: 'running', playerCount: 2, owner: 'PlayerA' },
+    { id: 2, status: 'full', playerCount: 4, owner: 'PlayerB' },
+    { id: 3, status: 'waiting for players', playerCount: 2, owner: 'PlayerC' },
+    { id: 4, status: 'waiting for players', playerCount: 1, owner: 'xyz' },
 ];
