3913c265ba7c10728154389b0d393aac73fa8176
Nik Zaugg <nik.zaugg@bf.uzh.ch>
2017-03-21 16:23:55 +0100

    #7: created table with dummy data

---
+++ src/app/shared/models/game.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by nikza on 21.03.2017.
+ */
+export class Game {
+    id: number;
+    playerCount: number;
+    status: string;
+    owner: number;
+}
---
+++ src/app/shared/models/mock-games.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by nikza on 21.03.2017.
+ */
+import {Game} from "./game";
+
+
+export const GAMES: Game[] = [
+    { id: 1, status: 'running', playerCount: 2 },
+    { id: 2, status: 'full',playerCount: 4 },
+    { id: 3, status: 'waiting for players',playerCount: 2 },
+    { id: 4, status: 'waiting for players',playerCount: 1 },
+];
