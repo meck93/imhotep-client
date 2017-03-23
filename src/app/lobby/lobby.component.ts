79228f8e30c5f04967206765ecb71816ff9e8cee
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2017-03-23 21:43:12 +0100

    #20: games with less than 2 players can not be started

--- src/app/lobby/lobby.component.ts
+++ src/app/lobby/lobby.component.ts
@@ -13,8 +13,8 @@ import Timer = NodeJS.Timer;
 export class LobbyComponent implements OnInit {
     games: Game[];
     user: User;
-    private timoutInterval: number = 1000;
-    private timoutId:Timer;
+    private timoutInterval: number = 10000;
+    private timoutId: Timer;
 
     constructor(private gameService: GameService) {
     }
@@ -27,9 +27,9 @@ export class LobbyComponent implements OnInit {
         this.user = JSON.parse(localStorage.getItem('currentUser'));
 
         var that = this;
-       this.timoutId = setInterval(function () {
-           that.getGames();
-       }, this.timoutInterval)
+        this.timoutId = setInterval(function () {
+            that.getGames();
+        }, this.timoutInterval)
     }
 
     ngOnDestrory(): void {
@@ -39,11 +39,13 @@ export class LobbyComponent implements OnInit {
     // get list of games
     getGames(): void {
         //this.gameService.getGames().then(games => this.games = games);
-       this.gameService.getGames()
-        .subscribe(games => {this.games = games;})
+        this.gameService.getGames()
+            .subscribe(games => {
+                this.games = games;
+            })
     }
 
-    // check whether a game is full or running
+    // check whether user may join a game
     isJoinable(status: String): boolean {
         // check if user owns one of the games
         for (var i = 0; i < this.games.length; i++) {
@@ -52,9 +54,11 @@ export class LobbyComponent implements OnInit {
             }
         }
 
-        if(status === 'RUNNING') {
+        // check if game is running
+        if (status === 'RUNNING') {
             return false;
         }
+
         return true;
     }
 
@@ -64,43 +68,30 @@ export class LobbyComponent implements OnInit {
         return owner === this.user.username;
     }
 
-  /** This function is only for demonstration. It shows the behaviour of adding a new game.
-   *  Later the add(name: string) function should trigger a POST request and register
-   *  a new game on the server.
-   *
-   * @param name the name of the newly added game
-   */
-  add(name: string): void {
-    name = name.trim();
-    if (!name) { return; }
-    var newGame = new Game();
-    newGame.id = this.games.length+1;
-    newGame.name = name;
-    newGame.status = 'PENDING';
-    newGame.amountOfPlayers = 1;
-    this.games.push(newGame);
-  }
+    // check if game has enough players to start
+    hasEnoughPlayers(numberOfPlayers: number) {
+        let minimum = 2;
+
+        return numberOfPlayers >= 2;
+    }
+
+    /** This function is only for demonstration. It shows the behaviour of adding a new game.
+     *  Later the add(name: string) function should trigger a POST request and register
+     *  a new game on the server.
+     *
+     * @param name the name of the newly added game
+     */
+    add(name: string): void {
+        name = name.trim();
+        if (!name) {
+            return;
+        }
+        var newGame = new Game();
+        newGame.id = this.games.length + 1;
+        newGame.name = name;
+        newGame.status = 'PENDING';
+        newGame.amountOfPlayers = 1;
+        this.games.push(newGame);
+    }
 
 }
-
-/*
-
-CODE FOR CHECKING IF USER OWNS A GAME WHEN LOBBY IS LOADED TO LATER DISABLE ALL JOIN BUTTONS
-
- // check if user owns one of the games
- for (var i = 0; i < this.games.length; i++) {
- if (this.games[i].owner === this.user.username) {
- this.isGameOwner = true;
- break;
- }
- }
-
-// ADD THIS TO IS jOINABLE METHOD
- if (this.isGameOwner) {
- return false;
- }
-
-
-
- */
-
