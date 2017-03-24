5e31ad35c3989dde42eca36dd5a1e63d3cc16e7a
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2017-03-24 20:18:13 +0100

    #30 first draft of ships with stones

--- src/app/game/ship/ship.component.ts
+++ src/app/game/ship/ship.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ship.component.css']
 })
 export class ShipComponent implements OnInit {
-
-  constructor() { }
+  occupied: boolean[];
 
   ngOnInit() {
+    this.occupied = [false, false, false];
+  }
+
+  setStone(number:number) {
+    this.occupied[number] = true;
   }
 
 }
---
+++ src/app/shared/models/ship.ts
@@ -0,0 +1,4 @@
+export class Ship {
+    public MAX_STONES:number;
+    public MIN_STONES:number;
+}
