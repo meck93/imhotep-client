f369da16c7e103c19b2a94eec4af97ba41026d56
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2017-03-29 19:35:52 +0200

    #31: added supply-sled images to component with variable

--- src/app/game/supply-sled/supply-sled.component.ts
+++ src/app/game/supply-sled/supply-sled.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'supply-sled',
   templateUrl: './supply-sled.component.html',
   styleUrls: ['./supply-sled.component.css']
 })
-export class SupplySledComponent implements OnInit {
+export class SupplySledComponent implements OnInit {// input variable for component
+  @Input() color: String = '';
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
