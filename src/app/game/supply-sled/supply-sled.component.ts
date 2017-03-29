420f1c325b76214587b4fc1883a1158c026b8649
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2017-03-29 18:40:29 +0200

    #31: changed stone-sled component to supply-sled and added images

---
+++ src/app/game/supply-sled/supply-sled.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'supply-sled',
+  templateUrl: './supply-sled.component.html',
+  styleUrls: ['./supply-sled.component.css']
+})
+export class SupplySledComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
