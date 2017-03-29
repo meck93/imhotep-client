2c049bce633e8bab963a66b826217a0683ac4c90
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2017-03-29 17:39:48 +0200

    added api url to environment: see comment for description

--- src/app/shared/services/ship.service.ts
+++ src/app/shared/services/ship.service.ts
@@ -2,17 +2,14 @@ import {Injectable, isDevMode} from '@angular/core';
 import {Http, Headers, RequestOptions, Response} from "@angular/http";
 import {Observable} from "rxjs";
 import {Ship} from "../models/ship";
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class ShipService {
     private apiUrl: string;
 
     constructor(private http: Http) {
-        if (isDevMode()) {
-            this.apiUrl = 'http://localhost:8080';
-        } else {
-            this.apiUrl = 'https://sopra-fs17-group09.herokuapp.com';
-        }
+        this.apiUrl = environment.apiUrl;
     }
 
     // TODO: Implement place stone method
