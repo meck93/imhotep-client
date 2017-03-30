03eb98beaae6f7ba1326a6c1dc4933fadae583d5
Nik Zaugg <nik.zaugg@bf.uzh.ch>
2017-03-31 00:10:02 +0200

    35: empty template for POLLING & CSS styles

--- src/app/shared/services/score-board/score-board.service.ts
+++ src/app/shared/services/score-board/score-board.service.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@angular/core';
+import {Injectable, isDevMode} from '@angular/core';
+import {Http, Headers, RequestOptions, Response, URLSearchParams} from "@angular/http";
+import {Observable} from "rxjs";
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+import { environment } from '../../../../environments/environment';
+
+import {Player} from '../../models/player';
+
+
+
 
 @Injectable()
 export class ScoreBoardService {
 
-  constructor() { }
+  private apiUrl:string;
+
+  constructor(private http: Http) {
+    this.apiUrl = environment.apiUrl;
+  }
+
+  // sets headers for the http requests
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+
+  /** GET request
+   *  gets a array containing all updated players of the current game
+   *
+   * @param gameId
+   * @returns {Observable<Player[]>}
+   */
+  updatePoints(gameId:number): Observable<Player[]> {
+    return this.http.get(this.apiUrl +'/games' + `/${gameId}`)
+        .map((response: Response) => response.json());
+  }
 
 }
