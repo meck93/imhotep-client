8902edfb52e3d7c4db78ee2f7a8e401a28c2acea
Nik Zaugg <nik.zaugg@bf.uzh.ch>
2017-04-05 00:16:36 +0200

    #25: display mock-cards with colors

--- src/app/game/market-place/market-place.component.ts
+++ src/app/game/market-place/market-place.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Stone} from '../../shared/models/stone';
+import {MarketCard} from '../../shared/models/market-card';
+import {MARKETCARDS} from '../../shared/models/mock-market-cards';
+import {Game} from '../../shared/models/game';
+import {MarketPlace} from '../../shared/models/marketPlace';
+import Timer = NodeJS.Timer;
+import {MarketPlaceService} from "../../shared/services/market-place/market-place.service";
 
 @Component({
   selector: 'market-place',
   templateUrl: './market-place.component.html',
-  styleUrls: ['./market-place.component.css']
+  styleUrls: ['./market-place.component.css'],
+  providers: [MarketPlaceService]
 })
 export class MarketPlaceComponent implements OnInit {
+  game:Game;
+  market: MarketPlace;
+  cards:MarketCard[];
 
-  constructor() { }
+  constructor(private marketPlaceService: MarketPlaceService) { }
 
   ngOnInit() {
+    this.game = JSON.parse(localStorage.getItem('currentGame'));
+    this.cards = MARKETCARDS;
   }
 
   displayRule():void{
@@ -17,5 +30,8 @@ export class MarketPlaceComponent implements OnInit {
     var popup = document.getElementById("marketPlacePopup");
     popup.classList.toggle("show");
   }
+
+  //TODO: implement event "unload stones at market"
+  takeCard():void{ }
 
 }
