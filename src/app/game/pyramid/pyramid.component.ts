2ef74494473c09027a606777db25e0065f3bd7cf
Nik Zaugg <nik.zaugg@bf.uzh.ch>
2017-04-04 13:31:11 +0200

    #26: pyramid polling

--- src/app/game/pyramid/pyramid.component.ts
+++ src/app/game/pyramid/pyramid.component.ts
@@ -3,28 +3,44 @@ import {Stone} from '../../shared/models/stone';
 import {MOCKSTONES} from '../../shared/models/mock-stones';
 import {Game} from '../../shared/models/game';
 import {BuildingSite} from '../../shared/models/buildingSite';
-//import {PyramidService} from "../../shared/services/burial-chamber/pyramid.service"; /*TODO HAS TO BE CREATED
 import Timer = NodeJS.Timer;
+import {PyramidService} from "../../shared/services/pyramid/pyramid.service";
 
 @Component({
     selector: 'pyramid',
     templateUrl: './pyramid.component.html',
-    styleUrls: ['./pyramid.component.css']
+    styleUrls: ['./pyramid.component.css'],
+    providers: [PyramidService]
 })
 export class PyramidComponent implements OnInit {
 
     game: Game; // current game
     pyramid: BuildingSite;
-    stones: Stone[] = MOCKSTONES; // temporary, replaced by service
+    //stones: Stone[] = MOCKSTONES; // temporary, replaced by service
+    stones: Stone[];
     pyramidStones: Stone[][][] = [];
     additionalStones: Stone[] = [];
     stoneCounter: number = 0;
 
+    private timoutInterval: number = 3000;
+    private timoutId: Timer;
 
-    constructor() {
+
+    constructor(private pyramidService: PyramidService) {
     }
 
     ngOnInit() {
+        //console.log("in Game Screen");
+        this.game = JSON.parse(localStorage.getItem('currentGame'));
+        this.updatePyramidStones();
+
+        var that = this;
+        this.timoutId = setInterval(function () {
+            that.updatePyramidStones();
+        }, this.timoutInterval)
+    }
+
+    arrangePyramidLayers(stones:Stone[]){
         // size of a pyramid layer (from bottom to top, e.g. 3 means layer has capacity of 3x3 stones)
         let layerSize = [3, 2, 1];
 
@@ -38,10 +54,10 @@ export class PyramidComponent implements OnInit {
         // iterate trough all layers and place stones
         for(let layer=0; layer<layerSize.length; layer++) {
             // check if there are some stones left to place on this layer
-            amountOfStones = (this.stones.length >= layerSize[layer]*layerSize[layer]) ? layerSize[layer]*layerSize[layer] : this.stones.length;
+            amountOfStones = (stones.length >= layerSize[layer]*layerSize[layer]) ? layerSize[layer]*layerSize[layer] : stones.length;
             if (amountOfStones > 0) {
                 // get the number of stones to place on this layer
-                layerStones = this.stones.splice(0, amountOfStones);
+                layerStones = stones.splice(0, amountOfStones);
 
                 // place stones on this layer
                 this.pyramidStones[layer] = this.arrangeStonesOnLayer(layerStones, layerSize[layer]);
@@ -60,5 +76,23 @@ export class PyramidComponent implements OnInit {
         }
 
         return layerArray;
+    }
+
+    // Updates the stones-array via a GET request to the server
+    updatePyramidStones(): void {
+        //console.log("updating burial chamber");
+        this.pyramidService.updatePyramidStones(this.game.id)
+            .subscribe(BuildingSite => {
+                if (BuildingSite) {
+                    // updates the stones array in this component
+                    this.pyramid = BuildingSite;
+                    this.stones = this.pyramid.stones;
+                    console.log(this.pyramid.stones);
+
+                    this.arrangePyramidLayers(this.stones);
+                } else {
+                    console.log("no games found");
+                }
+            })
     }
 }
--- src/app/shared/services/pyramid/pyramid.service.ts
+++ src/app/shared/services/pyramid/pyramid.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable, isDevMode} from '@angular/core';
+import {Http, Headers, RequestOptions, Response, URLSearchParams} from "@angular/http";
+import {Observable} from "rxjs";
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+import {BuildingSite} from '../../models/buildingSite';
+import { environment } from '../../../../environments/environment';
 
 @Injectable()
 export class PyramidService {
+  private apiUrl:string;
 
-  constructor() { }
+  constructor(private http: Http) {
+    this.apiUrl = environment.apiUrl;
+  }
 
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+
+  updatePyramidStones(gameId:number):Observable<BuildingSite>{
+    return this.http.get(this.apiUrl +'/games/'+`${gameId}`+ '/PYRAMID')
+        .map((response: Response) => response.json());
+  }
 }
