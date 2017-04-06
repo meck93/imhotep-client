2e79de5f8a8237517bf48e5dbe6d19a1a66cd513
Alex Scheitlin <alex.scheitlin@uzh.ch>
2017-04-06 08:26:23 +0200

    #31: show adding stone onption for corresponding supply sled (local storage)

--- src/app/game/supply-sled/supply-sled.component.ts
+++ src/app/game/supply-sled/supply-sled.component.ts
@@ -1,21 +1,57 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
-  selector: 'supply-sled',
-  templateUrl: './supply-sled.component.html',
-  styleUrls: ['./supply-sled.component.css']
+    selector: 'supply-sled',
+    templateUrl: './supply-sled.component.html',
+    styleUrls: ['./supply-sled.component.css']
 })
 export class SupplySledComponent implements OnInit {// input variable for component
-  @Input() color: String = '';
+    // color of this supply sled
+    @Input() color: String = '';
 
-  constructor() {
+    // player number and color of logged in user/player
+    playerNumber: number;
+    playerColor: string;
 
-  }
+    // TODO: needs to be initialized to 0 as soon as game sets the field current player correctly
+    currentPlayer: number = 1;
 
-  ngOnInit() {
-  }
+    constructor() {
 
-  getStones() {
+    }
 
-  }
+    ngOnInit() {
+        // get user id from local storage
+        let user = JSON.parse(localStorage.getItem('currentUser'));
+        let userId = user.id;
+
+        // get players of game form local storage
+        let game = JSON.parse(localStorage.getItem('currentGame'));
+        let players = game.players;
+
+        // get current player of the game
+        if (game.currentPlayer != undefined) {
+            this.currentPlayer = game.currentPlayer;
+        }
+
+        // find player number and color of this player
+        let player;
+        for (let i = 0; i < players.length; i++) {
+            if (players[i].id == userId) {
+                player = players[i];
+                break;
+            }
+        }
+        this.playerNumber = player.playerNumber;
+        this.playerColor = player.color;
+
+        console.log("sled color: " + this.color);
+        console.log("player color: " + this.playerColor);
+        console.log("current player: " + this.currentPlayer);
+        console.log("player number: " + this.playerNumber);
+    }
+
+    getStones() {
+
+    }
 }
