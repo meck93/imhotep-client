e5beaa7fc1f00d416dc847b61a9f8340287b2404
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2017-04-07 14:21:25 +0200

    #57, #58: removed current game from scoreboard and added game id from local storage

--- src/app/game/score-board/score-board.component.ts
+++ src/app/game/score-board/score-board.component.ts
@@ -1,82 +1,95 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import {ScoreBoardService} from '../../shared/services/score-board/score-board.service';
-import { Game } from '../../shared/models/game';
-import { Player } from '../../shared/models/player';
-let $ = require('../../../../node_modules/jquery/dist/jquery.slim.js');
+import {Component, OnInit, AfterViewInit} from '@angular/core';
 
+// polling
 import Timer = NodeJS.Timer;
 
-declare var jQuery:any;
+// services
+import {ScoreBoardService} from '../../shared/services/score-board/score-board.service';
+
+// models
+import {Player} from '../../shared/models/player';
+import {Game} from '../../shared/models/game';
+
+// others
+let $ = require('../../../../node_modules/jquery/dist/jquery.slim.js');
+declare let jQuery: any;
 
 @Component({
-  selector: 'score-board',
-  templateUrl: './score-board.component.html',
-  styleUrls: ['./score-board.component.css'],
-  providers: [ScoreBoardService]
+    selector: 'score-board',
+    templateUrl: './score-board.component.html',
+    styleUrls: ['./score-board.component.css'],
+    providers: [ScoreBoardService]
 })
-export class ScoreBoardComponent implements OnInit, AfterViewInit  {
+export class ScoreBoardComponent implements OnInit, AfterViewInit {
+    // polling
+    private timeoutId: Timer;
+    private timeoutInterval: number = 2000;
 
-  game:Game; // current game
-  players: Player[]; // players of the current game
+    game: Game; // current game
+    players: Player[]; // players of the current game
 
-  private timoutInterval: number = 2000;
-  private timoutId: Timer;
+    gameId: number;
 
-  constructor(private scoreBoardService:ScoreBoardService) {
-
-  }
+    //TODO: implement polling (service or local storage)
+    roundCounter: number = 1;
 
 
-  ngOnInit(): void {
-    this.game = JSON.parse(localStorage.getItem('currentGame'));
-    this.updatePoints(this.game.id);
+    constructor(private scoreBoardService: ScoreBoardService) {
 
-    /* POLLING */
-    var that = this;
-    this.timoutId = setInterval(function () {
+    }
 
-      that.updatePoints(that.game.id);
 
-    }, this.timoutInterval)
-  }
+    ngOnInit(): void {
+        // get game id from local storage
+        let game = JSON.parse(localStorage.getItem('game'));
+        this.gameId = game.id;
 
-  ngAfterViewInit():void{
-    var clicked=true;
-    $("#ScoreBoardDropDownClicker").on('click', function(){
-      if(clicked)
-      {
-        clicked=false;
-        $("#scoreBoard").css({"top": 0});
-      }
-      else
-      {
-        clicked=true;
-        $("#scoreBoard").css({"top": "-200px"});
-      }
-    });
+        this.updatePoints(this.gameId);
 
-    $(document).click(function(){
-      clicked = true;
-      $("#scoreBoard").css({"top": "-200px"});
-    });
+        /* POLLING */
+        var that = this;
+        this.timeoutId = setInterval(function () {
 
-    $("#ScoreBoardDropDownClicker").click(function(e){
-      e.stopPropagation();
-    });
+            that.updatePoints(that.gameId);
 
-  }
+        }, this.timeoutInterval)
+    }
 
-  // gets the updated Players and their points
-  updatePoints(gameId:number):void{
-    this.scoreBoardService.updatePoints(gameId)
-        .subscribe(players => {
-          if (players) {
-            // updates the players array in this component
-            this.players = players;
-          } else {
-            console.log("no players found");
-          }
-        })
-  }
+    ngAfterViewInit(): void {
+        var clicked = true;
+        $("#ScoreBoardDropDownClicker").on('click', function () {
+            if (clicked) {
+                clicked = false;
+                $("#scoreBoard").css({"top": 0});
+            }
+            else {
+                clicked = true;
+                $("#scoreBoard").css({"top": "-200px"});
+            }
+        });
+
+        $(document).click(function () {
+            clicked = true;
+            $("#scoreBoard").css({"top": "-200px"});
+        });
+
+        $("#ScoreBoardDropDownClicker").click(function (e) {
+            e.stopPropagation();
+        });
+
+    }
+
+    // gets the updated Players and their points
+    updatePoints(gameId: number): void {
+        this.scoreBoardService.updatePoints(gameId)
+            .subscribe(players => {
+                if (players) {
+                    // updates the players array in this component
+                    this.players = players;
+                } else {
+                    console.log("no players found");
+                }
+            })
+    }
 
 }
