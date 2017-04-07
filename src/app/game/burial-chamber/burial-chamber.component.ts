52182b4693d8f1ef668c3c5d90aa078d81921e2e
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2017-04-07 15:08:56 +0200

    #59: burial chamber clean up

--- src/app/game/burial-chamber/burial-chamber.component.ts
+++ src/app/game/burial-chamber/burial-chamber.component.ts
@@ -1,10 +1,15 @@
 import {Component, OnInit} from '@angular/core';
-import {Stone} from '../../shared/models/stone';
-import {Game} from '../../shared/models/game';
-import {BuildingSite} from '../../shared/models/buildingSite';
-import {BurialChamberService} from "../../shared/services/burial-chamber/burial-chamber.service";
+
+// polling
 import Timer = NodeJS.Timer;
 
+// services
+import {BurialChamberService} from "../../shared/services/burial-chamber/burial-chamber.service";
+
+// models
+import {BuildingSite} from '../../shared/models/buildingSite';
+import {Game} from '../../shared/models/game';
+import {Stone} from '../../shared/models/stone';
 
 @Component({
     selector: 'burial-chamber',
@@ -17,60 +22,45 @@ export class BurialChamberComponent implements OnInit {
     private timeoutId: Timer;
     private timeoutInterval: number = 3000;
 
-    // #newWay
+    // local storage data
     gameId: number;
 
-    burialChamber: BuildingSite; // building site object
-    rows: Stone[][] = []; // the rows for the stones on this building site
-    nrOfRows: number; // in how many rows the stones are split into
-
-
+    // component fields
+    burialChamber: BuildingSite;        // building site object
+    rows: Stone[][] = [];               // the rows for the stones on this building site
+    nrOfRows: number;                   // in how many rows the stones are split into
 
     constructor(private burialChamberService: BurialChamberService) {
+
     }
 
+    // initialize component
     ngOnInit() {
-        // #newWay
         // get game id from local storage
         let game = JSON.parse(localStorage.getItem('game'));
         this.gameId = game.id;
 
         // get the stones from the server
-        this.updateBurialChamberStones();
+        this.updateBurialChamber();
 
-        /*POLLING*/
-        var that = this;
+        // polling
+        let that = this;
         this.timeoutId = setInterval(function () {
-            that.updateBurialChamberStones();
+            that.updateBurialChamber();
         }, this.timeoutInterval)
     }
 
-    displayRule():void{
-        // displays the rules popup
-        var popup = document.getElementById("myPopup");
-        popup.classList.toggle("show");
+    // TODO: ensure component will be destroyed when changing to the winning screen
+    // destroy component
+    ngOnDestroy(): void {
+        // kill the polling
+        clearInterval(this.timeoutId);
     }
 
-    // places the current stones in rows for the component to display in the html
-    arrangeStones(stones: Stone[]): void {
-        // temporary stone array
-        let tempArray: Stone[][] = [];
-        this.nrOfRows = (stones.length / 3);
-
-        for (var i = 0; i < this.nrOfRows; i++) {
-            //splits the array into pieces of 3
-            var oneRowArray = stones.splice(0, 3);
-            tempArray.push(oneRowArray);
-        }
-
-        this.rows = tempArray;
-
-    }
-
-    //TODO: handle error
-    // Updates the stones-array via a GET request to the server
-    updateBurialChamberStones(): void {
-        //console.log("updating burial chamber");
+    // TODO: handle error
+    // updates the stones-array via a GET request to the server
+    updateBurialChamber(): void {
+        // console.log("updating burial chamber");
         this.burialChamberService.updateBurialChamberStones(this.gameId)
             .subscribe(BuildingSite => {
                 if (BuildingSite) {
@@ -83,6 +73,29 @@ export class BurialChamberComponent implements OnInit {
             })
     }
 
+    // places the current stones in rows for the component to display in the html
+    arrangeStones(stones: Stone[]): void {
+        // temporary stone array
+        let tempArray: Stone[][] = [];
+        this.nrOfRows = (stones.length / 3);
+
+        for (let i = 0; i < this.nrOfRows; i++) {
+            // splits the array into pieces of 3
+            let oneRowArray = stones.splice(0, 3);
+            tempArray.push(oneRowArray);
+        }
+
+        this.rows = tempArray;
+
+    }
+
+    // *************************************************************
+    // HELPER FUNCTIONS FOR UI
+    // *************************************************************
+
+    // displays the rules popup
+    displayRule(): void {
+        let popup = document.getElementById("myPopup");
+        popup.classList.toggle("show");
+    }
 }
-
-
