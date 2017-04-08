dac158992d9335b986f9e110374c045661ad6b7a
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2017-04-08 19:50:02 +0200

    #61: restructured pyramid code to later make state change visible

--- src/app/game/pyramid/pyramid.component.ts
+++ src/app/game/pyramid/pyramid.component.ts
@@ -26,16 +26,13 @@ export class PyramidComponent implements OnInit {
 
     // local storage data
     gameId: number;
+    numberOfPlayers: number;
 
     // component fields
-    pyramid: BuildingSite;
-    stones: Stone[];
     pyramidStones: Stone[][][] = [];
-    additionalStones: Stone[] = [];
-    additionalBlackStones: number;
-    additionalWhiteStones: number;
-    additionalGrayStones: number;
-    additionalBrownStones: number;
+    additionalStones: number[] = [];
+
+    hasAdditionalStones: boolean = false;
 
     stoneCounter: number = 0;
 
@@ -48,6 +45,7 @@ export class PyramidComponent implements OnInit {
         // get game id from local storage
         let game = JSON.parse(localStorage.getItem('game'));
         this.gameId = game.id;
+        this.numberOfPlayers = game.numberOfPlayers;
 
         this.updatePyramid();
 
@@ -69,25 +67,36 @@ export class PyramidComponent implements OnInit {
         this.pyramidService.updatePyramidStones(this.gameId)
             .subscribe(BuildingSite => {
                 if (BuildingSite) {
-                    // updates the stones array in this component
-                    this.pyramid = BuildingSite;
-                    this.stones = this.pyramid.stones;
-                    this.arrangePyramidLayers(this.stones);
+                    // retrieve data
+                    let pyramid = BuildingSite;
+
+                    // update local data
+                    this.updateData(pyramid);
                 } else {
                     console.log("no games found");
                 }
             })
     }
 
+    // update data and make changes visible to the user
+    updateData(pyramid: BuildingSite): void {
+        let stones = pyramid.stones;
+
+        // place stones at the pyramid
+        let additionalStones = this.arrangePyramidLayers(stones);
+
+        // place all additional stones sorted to their color
+        if (additionalStones.length > 0) {
+            this.hasAdditionalStones = true;
+            this.arrangeAdditionalStones(additionalStones);
+        }
+    }
+
     // *************************************************************
     // HELPER FUNCTIONS
     // *************************************************************
 
-    arrangePyramidLayers(stones:Stone[]){
-        if(stones.length > 14){
-            this.additionalStones = stones.splice(14,stones.length);
-            this.arrangeAdditionalStones(this.additionalStones);
-        }
+    arrangePyramidLayers(stones: Stone[]): Stone[] {
         // size of a pyramid layer (from bottom to top, e.g. 3 means layer has capacity of 3x3 stones)
         let layerSize = [3, 2, 1];
 
@@ -99,9 +108,9 @@ export class PyramidComponent implements OnInit {
 
         // place all stones on the pyramid layers
         // iterate trough all layers and place stones
-        for(let layer=0; layer<layerSize.length; layer++) {
+        for (let layer = 0; layer < layerSize.length; layer++) {
             // check if there are some stones left to place on this layer
-            amountOfStones = (stones.length >= layerSize[layer]*layerSize[layer]) ? layerSize[layer]*layerSize[layer] : stones.length;
+            amountOfStones = (stones.length >= layerSize[layer] * layerSize[layer]) ? layerSize[layer] * layerSize[layer] : stones.length;
             if (amountOfStones > 0) {
                 // get the number of stones to place on this layer
                 layerStones = stones.splice(0, amountOfStones);
@@ -110,9 +119,12 @@ export class PyramidComponent implements OnInit {
                 this.pyramidStones[layer] = this.arrangeStonesOnLayer(layerStones, layerSize[layer]);
             }
         }
+
+        // return stones that did not fit into the pyramid
+        return stones;
     }
 
-    arrangeStonesOnLayer(stones: Stone[], size:number): Stone[][] {
+    arrangeStonesOnLayer(stones: Stone[], size: number): Stone[][] {
         let layerArray: Stone[][] = [];
         let nrOfRows = (stones.length / size);
 
@@ -125,40 +137,34 @@ export class PyramidComponent implements OnInit {
         return layerArray;
     }
 
-    arrangeAdditionalStones(stones:Stone[]):void{
-        let blackStones = 0;
-        let whiteStones = 0;
-        let grayStones = 0;
-        let brownStones = 0;
+    arrangeAdditionalStones(stones: Stone[]): void {
+        let additionalStones: number[] = [0, 0, 0, 0];
 
-        console.log(stones);
-
-        for(var i=0; i<stones.length;i++){
-            if(stones[i].color == 'BLACK'){
-                blackStones++;
-            }
-            if(stones[i].color == 'WHITE'){
-                whiteStones++;
-            }
-            if(stones[i].color == 'GRAY'){
-                grayStones++;
-            }
-            if(stones[i].color == 'BROWN'){
-                brownStones++;
+        for (let i = 0; i < stones.length; i++) {
+            switch (stones[i].color) {
+                case 'BLACK':
+                    additionalStones[0]++;
+                    break;
+                case 'WHITE':
+                    additionalStones[1]++;
+                    break;
+                case 'GRAY':
+                    additionalStones[2]++;
+                    break;
+                case 'BROWN':
+                    additionalStones[3]++;
+                    break;
             }
         }
 
-        this.additionalBlackStones = blackStones;
-        this.additionalWhiteStones = whiteStones;
-        this.additionalGrayStones= grayStones;
-        this.additionalBrownStones= brownStones;
+        this.additionalStones = additionalStones;
     }
 
     // *************************************************************
     // HELPER FUNCTIONS FOR UI
     // *************************************************************
 
-    displayRule():void{
+    displayRule(): void {
         console.log("I rule!");
         let popup = document.getElementById("pyramidPopup");
         popup.classList.toggle("show");
