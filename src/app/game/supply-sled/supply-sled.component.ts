b863c7959638da7ba189be26b1af30b6e2e876ad
Nik Zaugg <nik.zaugg@bf.uzh.ch>
2017-04-10 16:52:05 +0200

    #53: first functioning  GET_STONES move implemented

--- src/app/game/supply-sled/supply-sled.component.ts
+++ src/app/game/supply-sled/supply-sled.component.ts
@@ -2,18 +2,20 @@ import {Component, OnInit, Input} from '@angular/core';
 import {Stone} from '../../shared/models/stone';
 import {Player} from '../../shared/models/player';
 import {SupplySled} from '../../shared/models/supplySled';
-import {SupplySledService} from '../../shared/services/supply-sled/supply-sled.service'
+import {SupplySledService} from '../../shared/services/supply-sled/supply-sled.service';
+import {MoveService} from '../../shared/services/move/move.service';
 
 // polling
 import {componentPollingIntervall} from '../../../settings/settings';
 import Timer = NodeJS.Timer;
+import {GameService} from "../../shared/services/game/game.service";
 
 
 @Component({
     selector: 'supply-sled',
     templateUrl: './supply-sled.component.html',
     styleUrls: ['./supply-sled.component.css'],
-    providers: [SupplySledService]
+    providers: [SupplySledService, MoveService]
 })
 
 export class SupplySledComponent implements OnInit {
@@ -35,8 +37,9 @@ export class SupplySledComponent implements OnInit {
     // component fields
     sledStones: Stone[]=[];
     supplySledSize: number;
+    roundNr:number;
 
-    constructor(private supplySledService: SupplySledService ) {
+    constructor(private supplySledService: SupplySledService, private moveService: MoveService, private gameService: GameService) {
 
     }
 
@@ -45,6 +48,9 @@ export class SupplySledComponent implements OnInit {
         // get game id and number of players from local storage
         let game = JSON.parse(localStorage.getItem('game'));
         this.gameId = game.id;
+
+        // get roundNr for moves-function
+        this.getRoundNr();
 
         // get player number and color from local storage
         let player = JSON.parse(localStorage.getItem('player'));
@@ -71,6 +77,9 @@ export class SupplySledComponent implements OnInit {
             // get current player
             let game = JSON.parse(localStorage.getItem('game'));
             that.currentPlayer = game.currentPlayer;
+            // get roundNr
+            that.getRoundNr();
+            // update stones on the supply sled
             that.updateSupplySled();
         }, this.timeoutInterval)
     }
@@ -97,17 +106,38 @@ export class SupplySledComponent implements OnInit {
         })
     }
 
-    // TODO: implement move
-    getStones() {
+    // TODO: discuss if we can pass the roundNr from the game.component.ts instead of getting it here
+    // gets the current round Nr from the server
+    getRoundNr():void{
+        this.gameService.getGameFromId(this.gameId)
+            .subscribe(game => {
+                if (game) {
+                    // update roundNr
+                    this.roundNr = game.roundCounter;
+                } else {
+                    // request error
+                }
+            })
+    }
 
+    // TODO: implement move
+    getStones():void {
+    this.moveService.getStones(this.gameId, this.roundNr, this.clientPlayerNumber)
+        .subscribe(response => {
+            // get the created game as the joined game
+            console.log(response);
+        });
     }
 
     // *************************************************************
     // HELPER FUNCTIONS FOR UI
     // *************************************************************
 
-    // TODO: as soon as sleds of players have stones in it
+    // returns boolean if all 5 places on the supply sled is occupied
     isSledFull() {
+        if(this.sledStones.length == 5){
+            return true;
+        }
         return false;
     }
 }
--- src/app/shared/services/move/move.service.ts
+++ src/app/shared/services/move/move.service.ts
@@ -1,8 +1,43 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+
+// requests
+import {Http, Headers, RequestOptions, Response, URLSearchParams} from "@angular/http";
+import {Observable} from "rxjs";
+import {environment} from '../../../../environments/environment';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class MoveService {
+  private apiUrl: string;
 
-  constructor() { }
+  // sets headers for the http requests
+  private headers = new Headers({'Content-Type': 'application/json'});
 
+  constructor(private http: Http) {
+    this.apiUrl = environment.apiUrl;
+  }
+
+
+  getStones(gameId:number, roundNr:number, playerNr:number):Observable<String>{
+
+    let bodyString = JSON.stringify({
+      gameId: gameId,
+      roundNr: roundNr,
+      playerNr: playerNr,
+      type: "GET_STONES",
+      "moveType": "GET_STONES"
+    });
+
+    // Create a request option
+    let options = new RequestOptions({headers: this.headers});
+
+    const url = `/games/${gameId}/rounds/${roundNr}/moves`;
+
+    return this.http.post(this.apiUrl + url, bodyString, options)
+        .map((response: Response) => {
+          return response.json();
+        })
+        .catch((error: any) => Observable.throw(error.json().error || 'Server error')); //...errors if
+  }
 }
