969c7703870272b03bf8f53c859b520368b214a0
Nik Zaugg <nik.zaugg@bf.uzh.ch>
2017-04-13 16:32:08 +0200

    #74: new player-cards component

---
+++ src/app/game/supply-sled/player-cards/player-cards/player-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-cards',
+  templateUrl: './player-cards.component.html',
+  styleUrls: ['./player-cards.component.css']
+})
+export class PlayerCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
