bd866e4ef2d24dc2654b9609b8c7506a3a10af22
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2017-04-14 10:38:01 +0200

    #78: removed targetSite from ship model

--- src/app/shared/models/ship.ts
+++ src/app/shared/models/ship.ts
@@ -4,7 +4,6 @@ export class Ship {
     public id: number;
     public gameId: number;
     public hasSailed: boolean;
-    public targetSite: string;
     public stones: Stone[];
     public MAX_STONES: number;
     public MIN_STONES: number;
