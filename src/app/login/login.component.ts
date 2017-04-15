487b64a54780e241715990e7118d8c4deba2473c
Nik Zaugg <nik.zaugg@bf.uzh.ch>
2017-04-15 19:04:16 +0200

    disabled user lockout at login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -40,7 +40,7 @@ export class LoginComponent implements OnInit {
         this.showLoadingSign();
 
         // primitive login restriction
-        if (this.user.name.match("alschei|meck|nzaugg")) {
+        //if (this.user.name.match("alschei|meck|nzaugg")) {
         this._service.login(this.user)
             .subscribe(result => {
                 if (result) {
@@ -48,7 +48,7 @@ export class LoginComponent implements OnInit {
                 }
 
             }, error => this.errorMessage = "This username already exists. Please try another one");
-        }
+        //}
         this.hideLoadingSign();
     }
 
