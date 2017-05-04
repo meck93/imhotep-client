0f989f25f37b525c385414f72cb130ccd443af54
Nik Zaugg <nik.zaugg@bf.uzh.ch>
2017-05-04 23:33:23 +0200

    #114: moveLog refined refactoring and added getCard-Move

--- src/app/game/notification-board/notification-board.component.ts
+++ src/app/game/notification-board/notification-board.component.ts
@@ -42,7 +42,7 @@ export class NotificationBoardComponent implements OnInit, AfterViewInit {
     private getStonesMessage: string = ' got stones from the quarry ';
     private sailShipMessage: string = ' sailed a ship to the ';
     private getCardMessage: string = ' picked ';
-    private playCardMessage: string = ' played ';
+    private playCardMessage: string = ' played card ';
     private moveMessage: string = '';
 
     constructor(private notificationBoardService: NotificationBoardService) {
@@ -111,6 +111,30 @@ export class NotificationBoardComponent implements OnInit, AfterViewInit {
             })
     }
 
+    compareChanges(page: Page): void {
+        let changesMade: boolean = false;
+
+        if (page.content.length > 0) {
+            for (var i = 0; i < page.content.length; i++) {
+                if (page.content[i].id != this.stagedMessages[i].id) {
+                    changesMade = true;
+                    break;
+                }
+            }
+            if (changesMade) {
+                this.gameMessages = page.content;
+                this.stagedMessages = page.content;
+            }
+
+            if (this.gameMessages[0] != undefined) {
+                if (this.lastMove.id != this.gameMessages[0].id) {
+                    this.lastMove = page.content[0];
+                    this.updateLastMovePopup();
+                }
+            }
+        }
+    }
+
     updateMoveMessages(messages: PageElement[]){
         for(let i=0; i<messages.length; i++){
             switch (messages[i].moveType) {
@@ -145,7 +169,6 @@ export class NotificationBoardComponent implements OnInit, AfterViewInit {
                     }
                     break;
                 case 'GET_CARD':
-                    // TODO: make nice strings with switch case
                     switch(messages[i].marketCardType){
                         case 'ENTRANCE':
                             messages[i].moveMessage = this.getCardMessage + 'Entrance';
@@ -183,40 +206,32 @@ export class NotificationBoardComponent implements OnInit, AfterViewInit {
                     }
                     break;
                 case 'PLAY_CARD':
-                    // TODO: make nice strings with switch case
-                    messages[i].moveMessage = this.playCardMessage;
+                    switch(messages[i].marketCardType){
+                        case 'LEVER':
+                            messages[i].moveMessage = this.playCardMessage + 'Lever';
+                            break;
+                        case 'HAMMER':
+                            messages[i].moveMessage = this.playCardMessage + 'Hammer';
+                            break;
+                        case 'SAIL':
+                            messages[i].moveMessage = this.playCardMessage + 'Sail';
+                            break;
+                        case 'CHISEL':
+                            messages[i].moveMessage = this.playCardMessage + 'Chisel';
+                            break;
+                    }
                     break;
             }
         }
     }
 
-    compareChanges(page: Page): void {
-        let changesMade: boolean = false;
-
-        if (page.content.length > 0) {
-            for (var i = 0; i < page.content.length; i++) {
-                if (page.content[i].id != this.stagedMessages[i].id) {
-                    changesMade = true;
-                    break;
-                }
-            }
-            if (changesMade) {
-                this.gameMessages = page.content;
-                this.stagedMessages = page.content;
-            }
-
-            if (this.gameMessages[0] != undefined) {
-                if (this.lastMove.id != this.gameMessages[0].id) {
-                    this.lastMove = page.content[0];
-                    this.updateLastMovePopup();
-                }
-            }
-        }
-    }
-
+    /*************************************************/
+    /***************SHOW MOVE DETAILS****************/
+    /*************************************************/
     showMoveDetails(message: PageElement): void {
         this.detailMove = message;
         this.showMove = true;
+        this.highlightPlayerName(message.playerNr);
 
         switch (message.moveType) {
             case 'PLACE_STONE':
@@ -236,57 +251,78 @@ export class NotificationBoardComponent implements OnInit, AfterViewInit {
                 break;
         }
     }
-
     highlightPlaceStoneMove(message: PageElement): void {
         var $exists = $('#ship' + message.shipId).children().length > 0;
         if ($exists) {
-            let ship = document.getElementById("ship" + message.shipId);
-            let count = $('#ship' + message.shipId + ' .ship-middle .place').length;
-            let position = count - message.placeOnShip + 1;
-            $('.ship-container').css("opacity", "0.5");
-            $('.harbor-container').css("z-index", "600");
-            $('#supplySled' + message.playerNr + ' .player').css("z-index", "600");
-
-            ship.style.opacity = "1.0";
-            ship.style.backgroundColor = "grey";
-            ship.style.border = "1px solid lime";
-            ship.style.borderRadius = "5px";
-            ship.style.zIndex = "1000";
-
-            $('#ship' + message.shipId + ' .ship-middle .place:nth-child(' + position + ') .stone').css("border", "2px solid lime");
+            this.highlightHarbor();
+            this.highlightShip(message.shipId, message.placeOnShip);
         } else {
             this.isSailed = true;
-            $('#supplySled' + message.playerNr + ' .player').css("z-index", "600");
+        }
+    }
+    highlightGetStoneMove(message: PageElement): void {
+        this.highlightSupplySled(message.playerNr);
+    }
+    highlightSailMove(message: PageElement): void {
+    }
+    highlightGetCardMove(message: PageElement): void {
+    }
+    highlightPlayCardMove(message: PageElement): void {
+        switch(message.marketCardType){
+            case 'LEVER':
+                this.highlightPlayCardMove_LEVER(message);
+                break;
+            case 'HAMMER':
+                this.highlightPlayCardMove_HAMMER(message);
+                break;
+            case 'SAIL':
+                this.highlightPlayCardMove_SAIL(message);
+                break;
+            case 'CHISEL':
+                this.highlightPlayCardMove_CHISEL(message);
+                break;
         }
     }
 
-    highlightGetStoneMove(message: PageElement): void {
-        let sled = document.getElementById("supplySled" + message.playerNr);
-
-        sled.style.backgroundColor = "grey";
-        sled.style.border = "1px solid lime";
-        sled.style.borderRadius = "5px";
-        sled.style.zIndex = "1000";
+    highlightPlayCardMove_LEVER(message: PageElement):void{
+        this.isSailed = true;
     }
-
-    highlightSailMove(message: PageElement): void {
-        $('#supplySled' + message.playerNr + ' .player').css("z-index", "1000");
-        console.log("ship sailed");
+    highlightPlayCardMove_HAMMER(message: PageElement):void{
+        var $exists = $('#ship' + message.shipId).children().length > 0;
+        if ($exists) {
+            this.highlightHarbor();
+            this.highlightShip(message.shipId, message.placeOnShip);
+            this.highlightSupplySled(message.playerNr);
+        } else {
+            this.isSailed = true;
+        }
     }
-
-    highlightGetCardMove(message: PageElement): void {
-        $('#supplySled' + message.playerNr + ' .player').css("z-index", "1000");
+    highlightPlayCardMove_SAIL(message: PageElement):void{
+        this.isSailed = true;
     }
-
-    highlightPlayCardMove(message: PageElement): void {
-        $('#supplySled' + message.playerNr + ' .player').css("z-index", "1000");
+    highlightPlayCardMove_CHISEL(message: PageElement):void{
+        var $exists1 = $('#ship' + message.shipId).children().length > 0;
+        var $exists2 = $('#ship' + message.shipId2).children().length > 0;
+        if ($exists1 && $exists2) {
+            this.highlightHarbor();
+            this.highlightShip(message.shipId, message.placeOnShip);
+            this.highlightShip(message.shipId2, message.placeOnShip2);
+        }
+        else {
+            this.isSailed = true;
+        }
     }
+    /*************************************************/
 
 
 
+    /*************************************************/
+    /***************HIDE MOVE DETAILS****************/
+    /*************************************************/
     hideMoveDetails(message: PageElement): void {
         this.detailMove = null;
         this.showMove = false;
+        this.hidePlayerName(message.playerNr);
 
         switch (message.moveType) {
             case 'PLACE_STONE':
@@ -306,53 +342,63 @@ export class NotificationBoardComponent implements OnInit, AfterViewInit {
                 break;
         }
     }
-
     hidePlaceStoneMove(message: PageElement): void {
         var $exists = $('#ship' + message.shipId).children().length > 0;
         if ($exists) {
-            let ship = document.getElementById("ship" + message.shipId);
-            let count = $('#ship' + message.shipId + ' .ship-middle .place').length;
-            let position = count - message.placeOnShip + 1;
-            $('.ship-container').css("opacity", "1");
-            $('.harbor-container').css("z-index", "0");
-            $('#supplySled' + message.playerNr + ' .player').css("z-index", "20");
-
-            ship.style.opacity = "1.0";
-            ship.style.backgroundColor = "transparent";
-            ship.style.border = "none";
-            ship.style.borderRadius = "none";
-
-            $('#ship' + message.shipId + ' .ship-middle .place:nth-child(' + position + ') .stone').css("border", "none");
-            //$('#ship'+message.shipId+' .ship-middle .place:nth-child('+ position).css("border", "none");
+            this.hideHarbor();
+            this.hideShip(message.shipId, message.placeOnShip);
         } else {
-            $('#supplySled' + message.playerNr + ' .player').css("z-index", "20");
             this.isSailed = false;
         }
     }
-
     hideGetStoneMove(message: PageElement): void {
-        let sled = document.getElementById("supplySled" + message.playerNr);
-
-        sled.style.opacity = "1.0";
-        sled.style.backgroundColor = "transparent";
-        sled.style.border = "none";
-        sled.style.borderRadius = "none";
-        sled.style.zIndex = "auto";
+        this.hideSupplySled(message.playerNr)
     }
-
     hideSailMove(message: PageElement): void {
-        $('#supplySled' + message.playerNr + ' .player').css("z-index", "20");
     }
-
     hideGetCardMove(message: PageElement): void {
-        $('#supplySled' + message.playerNr + ' .player').css("z-index", "20");
     }
-
     hidePlayCardMove(message: PageElement): void {
-        $('#supplySled' + message.playerNr + ' .player').css("z-index", "20");
+        switch(message.marketCardType){
+            case 'LEVER':
+                this.hidePlayCardMove_LEVER(message);
+                break;
+            case 'HAMMER':
+                this.hidePlayCardMove_HAMMER(message);
+                break;
+            case 'SAIL':
+                this.hidePlayCardMove_SAIL(message);
+                break;
+            case 'CHISEL':
+                this.hidePlayCardMove_CHISEL(message);
+                break;
+        }
     }
 
+    hidePlayCardMove_LEVER(message: PageElement):void{
+        this.isSailed = false;
+    }
+    hidePlayCardMove_HAMMER(message: PageElement):void{
+        this.hideHarbor();
+        this.hideShip(message.shipId, message.placeOnShip);
+        this.hideSupplySled(message.playerNr);
+    }
+    hidePlayCardMove_SAIL(message: PageElement):void{
 
+    }
+    hidePlayCardMove_CHISEL(message: PageElement):void{
+        var $exists1 = $('#ship' + message.shipId).children().length > 0;
+        var $exists2 = $('#ship' + message.shipId2).children().length > 0;
+        if ($exists1 && $exists2) {
+            this.hideHarbor();
+            this.hideShip(message.shipId, message.placeOnShip);
+            this.hideShip(message.shipId2, message.placeOnShip2);
+        }
+        else {
+            this.isSailed = false;
+        }
+    }
+    /*************************************************/
 
     updateLastMovePopup(): void {
         if (!this.isHoveringPopup) {
@@ -374,5 +420,69 @@ export class NotificationBoardComponent implements OnInit, AfterViewInit {
         this.hideMoveDetails(lastMove);
         $("#lastMovePopup").addClass("step");
     }
+
+
+    /************************************************************/
+    /**************HELPER FUNCTIONS FOR HIGHLIGHTING*************/
+    /************************************************************/
+
+    highlightPlayerName(playerNr: number):void{
+        $('#supplySled' + playerNr + ' .player').css("z-index", "1000");
+    }
+    hidePlayerName(playerNr: number):void{
+        $('#supplySled' + playerNr + ' .player').css("z-index", "20");
+    }
+
+    highlightHarbor():void{
+        $('.ship-container').css("opacity", "0.5");
+        $('.harbor-container').css("z-index", "600");
+    }
+    hideHarbor():void{
+        $('.ship-container').css("opacity", "1");
+        $('.harbor-container').css("z-index", "0");
+    }
+
+    highlightShip(shipId:number, placeOnShip:number):void{
+        let ship = document.getElementById("ship" + shipId);
+        let count = $('#ship' + shipId + ' .ship-middle .place').length;
+        let position = count - placeOnShip + 1;
+        ship.style.opacity = "1.0";
+        ship.style.backgroundColor = "grey";
+        ship.style.border = "1px solid lime";
+        ship.style.borderRadius = "5px";
+        ship.style.zIndex = "1000";
+        $('#ship' + shipId + ' .ship-middle .place:nth-child(' + position + ') .stone').css("border", "2px solid lime");
+    }
+    hideShip(shipId:number, placeOnShip:number):void{
+        let ship = document.getElementById("ship" + shipId);
+        let count = $('#ship' + shipId + ' .ship-middle .place').length;
+        let position = count - placeOnShip + 1;
+        ship.style.opacity = "1.0";
+        ship.style.backgroundColor = "transparent";
+        ship.style.border = "none";
+        ship.style.borderRadius = "none";
+        ship.style.zIndex = "auto";
+
+        $('#ship' + shipId + ' .ship-middle .place:nth-child(' + position + ') .stone').css("border", "none");
+    }
+
+    highlightSupplySled(playerNr:number):void{
+        let sled = document.getElementById("supplySled" + playerNr);
+
+        sled.style.backgroundColor = "grey";
+        sled.style.border = "1px solid lime";
+        sled.style.borderRadius = "5px";
+        sled.style.zIndex = "1000";
+    }
+    hideSupplySled(playerNr:number):void{
+        let sled = document.getElementById("supplySled" + playerNr);
+
+        sled.style.opacity = "1.0";
+        sled.style.backgroundColor = "transparent";
+        sled.style.border = "none";
+        sled.style.borderRadius = "none";
+        sled.style.zIndex = "auto";
+    }
+
 
 }
